ad8624ce6208161988e24ecb058ef76fa8347762
Cidjohn <cidjohn31@gmail.com>
2024-11-16 12:41:24 +0800

    feature/sp3-86eqqqhtc/Content_Setting_Update

--- philbizz_api/views/cms/cms_views.py
+++ philbizz_api/views/cms/cms_views.py
@@ -45,12 +45,13 @@ class CMSView(APIView):
                 icon_image=textline['required']['image'],
                 location_image=textline['required']['location']
             )
-
-            for key in personInvolve['entries']:
-                name = key['personnelName']
-                position = key['position']
-                image = key['imagePreview']
-                ContentRepository.create_card_person(info_id=card_info.id, name=name, position=position, image=image)
+            if 'entries' in personInvolve and len(personInvolve['entries']) > 0:
+                for key in personInvolve['entries']:
+                    if len(key) > 0 : 
+                        name = key['personnelName']
+                        position = key['position']
+                        image = key['imagePreview']
+                        ContentRepository.create_card_person(info_id=card_info.id, name=name, position=position, image=image)
 
             for key in textline['option']:
                 value = textline['option'][key]['value']
@@ -65,6 +66,89 @@ class CMSView(APIView):
 
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class CSMUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        try:
+            treeview = request.data.get('Treeview')
+            textline = request.data.get('Textline')
+            texteditor = request.data.get('Texteditor')
+            personInvolve = request.data.get("Personnel")
+            
+            if not treeview or not textline or not texteditor:
+                return Response({"error": "Invalid or incomplete data"}, status=status.HTTP_400_BAD_REQUEST)
+
+            business = ContentRepository.get_business_by_header(treeview.get('name'))
+            if not business:
+                return Response({"error": "Business not found"}, status=status.HTTP_404_NOT_FOUND)
+
+            ContentRepository.update_card_settings(
+                id=treeview.get('uuid'),
+                location=treeview.get('child'),
+                title=textline['required'].get('title'),
+                images=treeview.get('imageTitle'),
+                description=textline['required'].get('address')
+            )
+
+            card_info = ContentRepository.update_card_info(
+                id=textline['required'].get('uuid'),
+                name=textline['required'].get('title'),
+                contact=textline['required'].get('contact'),
+                email=textline['required'].get('email'),
+                desc=textline['required'].get('description'),
+                content=texteditor,
+                servicetype=textline['required'].get('service'),
+                icon_image=textline['required'].get('image'),
+                location_image=textline['required'].get('location')
+            )
+
+            if personInvolve and 'entries' in personInvolve:
+                for key in personInvolve['entries']:
+                    if len(key) > 0:
+                        if isinstance(key, dict):
+                            id = key.get('uuid')
+                            name = key.get('personnelName')
+                            position = key.get('position')
+                            image = key.get('imagePreview')
+                            if id is None:
+                                ContentRepository.create_card_person(info_id=card_info.id, name=name, position=position, image=image)
+                            else:
+                                ContentRepository.update_person_involve(id=id, name=name, position=position, image=image)
+                        else:
+                            return Response({"error": "Invalid item in 'person details'"}, status=status.HTTP_400_BAD_REQUEST)
+
+            
+            if 'option' in textline and isinstance(textline['option'], list):
+                for option in textline['option']:
+                    if isinstance(option, dict):
+                        id = option.get('uuid')
+                        image_url = option.get('value')
+                        if id is None:
+                            ContentRepository.create_card_image(info_id=card_info.id, image_url=image_url)
+                        else:
+                            ContentRepository.update_card_image_link(id=id, image_url=image_url)
+                    else:
+                        return Response({"error": "Invalid item in 'image_link'"}, status=status.HTTP_400_BAD_REQUEST)
+
+            
+            if 'social' in textline:
+                for key in textline['social']:
+                    if isinstance(key, dict):
+                        id = key.get('uuid')
+                        media = key.get('social')
+                        value = key.get('link')
+                        if id is None:
+                            ContentRepository.create_card_social(info_id=card_info.id, social_media=media, social_value=value)
+                        else:
+                            ContentRepository.update_card_social(id=id, social_media=media, social_value=value)
+                    else:
+                        return Response({"error": "Invalid item in 'social'"}, status=status.HTTP_400_BAD_REQUEST)
+                        
+            return Response({"message": f"Update {treeview.get('name')} card successfully!"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": f"An unexpected error occurred: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
         
 class CSMViewList(APIView) :
     permission_classes = []
